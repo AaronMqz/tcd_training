bfb0174344acd8dc4b86026cca3e8c4a107d1dcb
Reymundo López <reymundolopez@gmail.com>
2013-11-04 07:39:37 -0600

    UI traduction start

--- 04ui/i18n/localizations.js
+++ 04ui/i18n/localizations.js
@@ -14,6 +14,28 @@ String.toLocaleString({
 	},
 	"ja": {
 		"%l_filetitle": "はじめに",
-		"%l_lessontitle": "はじめに",
+		"%l_lessontitle": "はじめに"
+	},
+	"es": {
+		"%l_filetitle": "Interfaz de usuario",
+		"%l_lessontitle": "Interfaz de usuario",
+		"%l_subtitle": "Titanium Certified Developer (TCD) Training",
+		"%l_slidenote_titleslide": "",
+		"%l_qa_slide": "Q&amp;A",
+		"%l_slidenote_qa": "",
+		"%l_agenda_title" : "En esta lección, usted:",
+		"%l_agenda1" : "Explorará los componentes usados para crear la UI",
+		"%l_agenda2" : "Aprenderá sus opciones para los diferentes componentes y 'layouts' de posicionamiento de UI",
+		"%l_agenda3" : "Manejar eventos generados y no generados por el usuario",
+		"%l_agenda4" : "Aprenderá como asignar a su aplicación un icono y una pantalla de inicio, además de internacionalizar su aplicación",
+		"%l_topic" : "Tema",
+		"%l_uictitle" : "Componentes de la Interfaz de Usuario (UI)",
+		"%l_controls" : "Controles",
+		"%l_controls1" : "Ventanas y vistas",
+		"%l_controls2" : "Etiquetas",
+		"%l_controls3" : "ImageViews",
+		"%l_controls4" : "Botones, cajas de texto, áreas de texto",
+		"%l_controls5" : "Controles de navegación -- pestañas, ventanas divididas, etc...",
+		"%l_controls6" : "Relacionados con multimedia (reproductores de video o audio)"
 	}
 });
